533d99f309353ce73edadec3befabca74b988dd4
Vigneshwaran Ravichandran <rvigneshwaran.tr@gmail.com>
2024-02-11 02:27:37 -0500

    Bug 7: Approving a transaction won't persist the new value

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ export function App() {
 
   const loadAllTransactions = useCallback(async () => {
     setIsLoading(true)
-    transactionsByEmployeeUtils.invalidateData()
+    //transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
     setIsLoading(false)
